f10c2cbb4788ef2f1df4e5abaf2a72418d2a18de
Bartosz Michalski <bmichalski86@gmail.com>
2023-11-09 00:01:46 +0100

    Problem 9 solved

--- ProjectEuler/Solver.fs
+++ ProjectEuler/Solver.fs
@@ -12,6 +12,7 @@ let private problems = [
   6, "Sum Square Difference", Problem006.solve
   7, "10001st Prime", Problem007.solve
   8, "Largest Product in a Series", Problem008.solve
+  9, "Special Pythagorean Triplet", Problem009.solve
 ]
 
 let private calculateResult (number, title, solve) =
