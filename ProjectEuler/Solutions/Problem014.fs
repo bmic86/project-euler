9545eede00dc023bc2902de4a548c1afaa691538
Bartosz Michalski <bmichalski86@gmail.com>
2023-11-13 01:31:25 +0100

    Problem 14 solved

---
+++ ProjectEuler/Solutions/Problem014.fs
@@ -0,0 +1,28 @@
+﻿// Problem 14: Longest Collatz Sequence.
+module ProjectEuler.Solutions.Problem014
+
+let private collatzSequenceLenght number =
+    let rec loop lenght currentNumber =
+        match currentNumber with
+        | 1L -> lenght
+        | num when num % 2L = 0L -> loop (lenght + 1) (num / 2L)
+        | num -> loop (lenght + 1) (3L * num + 1L)
+
+    loop 1 (int64 number)
+
+let private longestCollatzSequenceStartNumber limit =
+    let rec loop longestNumber maxLenght currentNumber =
+        match currentNumber with
+        | num when num <= 0 -> longestNumber
+        | num ->
+            let lenght = collatzSequenceLenght num
+
+            if lenght > maxLenght then
+                loop num lenght (num - 1)
+            else
+                loop longestNumber maxLenght (num - 1)
+
+    loop 0 0 (limit - 1)
+
+let solve () =
+    longestCollatzSequenceStartNumber 1_000_000 |> int64
--- ProjectEuler/Solver.fs
+++ ProjectEuler/Solver.fs
@@ -17,7 +17,8 @@ let private problems =
       10, "Summation of Primes", Problem010.solve
       11, "Largest Product in a Grid", Problem011.solve
       12, "Highly Divisible Triangular Number", Problem012.solve
-      13, "Large Sum", Problem013.solve ]
+      13, "Large Sum", Problem013.solve
+      14, "Longest Collatz Sequence", Problem014.solve ]
 
 let private calculateResult (number, title, solve) =
     let stopWatch = Stopwatch.StartNew()
